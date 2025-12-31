54defe6e6ea42addc737d9e43b3dc24d114e95a5
davidpoyade-netizen <david.poyade@gmail.com>
2025-12-31 16:24:42 +0100

    Add files via upload

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
 export const SUPABASE_URL = "https://pqtjvvvemypuhhvsoyrc.supabase.co";
-export const SUPABASE_ANON_KEY = "TON_ANON_PUBLIC_KEY";
+export const SUPABASE_ANON_KEY = "sb_publishable_iIXfzTXrNjdPavfxXX6jGQ_9nsnGNj_";
 
 export const supabase = createClient(sb_publishable_iIXfzTXrNjdPavfxXX6jGQ_9nsnGNj_);
