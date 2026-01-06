7bb1dddb6ec6f913ca90723b4da9c5470f9e5b5d
davidpoyade-netizen <david.poyade@gmail.com>
2026-01-06 18:40:09 +0100

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,7 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
 export const SUPABASE_URL = "https://pqtjvvvemypuhhvsoyrc.supabase.co";
-export const SUPABASE_ANON_KEY = "sb_publishable_iIXfzTXrNjdPavfxXX6jGQ_9nsnGNj_";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBxdGp2dnZlbXlwdWhodnNveXJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY2OTQ1MzksImV4cCI6MjA4MjI3MDUzOX0.KA62g7qLe17ZXpjzuCPj49c-s1mk428LrVwh3NsC2to";
 
 export const supabase = createClient(sb_publishable_iIXfzTXrNjdPavfxXX6jGQ_9nsnGNj_);
+
